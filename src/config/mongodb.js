7833de2832789821f1a0660c732c1069aee71102
Leonardo da Silva <leonardodasilva1915@gmail.com>
2023-03-06 08:43:44 -0300

    fix: mongodb connection

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,34 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root';
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'bolinho';
-const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
-const MONGO_PORT = process.env.MONGO_PORT || '27017';
-const MONGO_DATABASE = process.env.MONGO_DATABASE || 'star-data-base';
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root'
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'bolinho'
+const MONGO_HOST = process.env.MONGO_HOST || 'localhost'
+const MONGO_PORT = process.env.MONGO_PORT || '27017'
+const MONGO_DATABASE = process.env.MONGO_DATABASE || 'star-data-base'
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}?authSource=admin`;
 
-const connection = mongoose.createConnection(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  authSource: 'admin'
-});
-
-const connect = () => {
-  return new Promise((resolve, reject) => {
-    connection.on('open', () => {
-      console.log('MongoDB connected successfully');
-      resolve();
-    });
-    connection.on('error', (err) => {
-      console.log('MongoDB connection error:', err);
-      reject(err);
-    });
-  });
+const connect = async () => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('MongoDB connected successfully')
+  }).catch((err) => {
+    console.log('MongoDB connection error:', err)
+    throw err
+  })
 }
 
-
-module.exports = {
-  connection,
-  connect
-};
+module.exports = connect
